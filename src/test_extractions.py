8d497f411e19a7065107041741828a05281bce3c
clinteads <clint.eads@gmail.com>
2026-01-25 14:12:29 -0700

    added extractions

---
+++ src/test_extractions.py
@@ -0,0 +1,14 @@
+import unittest
+
+from extractions import extract_markdown_images, extract_markdown_links
+
+class TestExtractions(unittest.TestCase):
+    def test_extract_image(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")],matches)
